bcec1bada25a145601dbe27d124fbfd9b5f4a9cd
mina <minah0523@gmail.com>
2021-06-26 16:17:51 +0900

    add DetailScreen

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'
 import axios from 'axios';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
 
 const DetailScreen = ({match}) => {
 
@@ -19,9 +22,56 @@ const DetailScreen = ({match}) => {
     }, {})
 
     return (
-        <div>
-            <h1>{product.name}</h1>
-        </div>
+        <>
+            <Link to={'/'} className='btn btn-light my-3'>Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price : ${product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Price : </Col>
+                                <Col>
+                                    <strong>${product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Status : </Col>
+                                <Col>
+                                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                                Add to Cart
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+
+            </Row>
+        </>    
     );
 };
 
