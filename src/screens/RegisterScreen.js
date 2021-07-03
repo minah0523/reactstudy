889791b579168fbd40126ffc24480ffd6bf4c212
mina <minah0523@gmail.com>
2021-07-03 16:29:36 +0900

    add Register Function

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
+import axios from 'axios';
+
 
 const RegisterScreen = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const userInput = {
+            name,
+            email,
+            password
+        }
+
+        await axios.post("/api/users", userInput)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err.message))
+
+    }
+
+
     return (
         <FormContainer>
             <h1>Register</h1>
-            <Form>
+            <Form onSubmit={submitHandler}>
                 <Form.Group controlId="name">
                     <Form.Label>Name</Form.Label>
                     <Form.Control 
                         type='text'
                         placeholder='Enter User Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Group controlId="email">
@@ -20,6 +44,8 @@ const RegisterScreen = () => {
                     <Form.Control 
                         type='email'
                         placeholder='Enter Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Group controlId="password">
@@ -27,6 +53,8 @@ const RegisterScreen = () => {
                     <Form.Control 
                         type='password'
                         placeholder='Enter Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </Form.Group>
                 <Button type="submit" variant="primary">Register</Button>
