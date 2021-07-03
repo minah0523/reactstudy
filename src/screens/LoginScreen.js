6132aa1b322dadf645a0e2bf75170ca42a692cbf
mina <minah0523@gmail.com>
2021-07-03 16:19:57 +0900

    add Login Function

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
+import axios from 'axios';
+
 
 const LoginScreen = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+
+        const userInput = {
+            email,
+            password
+        }
+
+        await axios.post("/api/users/login", userInput)
+        .then(res => console.log(res))
+        .catch(err => console.log(err.message))
+    }
+
     return (
         <FormContainer>
             <h1>Sign In</h1>
-            <Form>
+            <Form onSubmit={submitHandler}>
                <Form.Group controlId="email">
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control 
                         type='email'
                         placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />   
                 </Form.Group>
                 <Form.Group controlId="password">
@@ -20,6 +41,8 @@ const LoginScreen = () => {
                     <Form.Control 
                         type='password'
                         placeholder='Enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />   
                 </Form.Group>
                 <Button type="submit" variant="primary">Sign In</Button>
